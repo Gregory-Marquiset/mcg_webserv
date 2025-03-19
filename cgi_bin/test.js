23929ecdd0c5c526d044c35f386eeb748ebbfa32
gmarquis <gmarquis@student.42.fr>
2025-03-19 11:47:27 +0100

    add cgi gestion

---
+++ cgi_bin/test.js
@@ -0,0 +1,3 @@
+#!/usr/bin/node
+console.log("Content-Type: text/html\n");
+console.log("<html><body><h1>CGI Test Node.js!</h1></body></html>");
---
+++ cgi_bin/test.php
@@ -0,0 +1,5 @@
+#!/usr/bin/php-cgi
+<?php
+header("Content-Type: text/html");
+echo "<html><body><h1>CGI Test PHP!</h1></body></html>";
+?>
