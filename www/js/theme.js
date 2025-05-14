cc7f0d064a58e10710ab3bca6cd9fa9b158e2d8e
gmarquis <gmarquis@student.42.fr>
2025-05-14 11:03:51 +0200

    theme corriger

--- www/js/theme.js
+++ www/js/theme.js
@@ -1,32 +1,33 @@
-function toggleTheme()
-{
-	const current = document.body.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
-	const newTheme = current === 'light-theme' ? 'dark-theme' : 'light-theme';
-	applyTheme(newTheme);
-	window.location.reload();
-}
-
-function getCookieValue(name)
+function	getCookieValue(name)
 {
 	const match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
 	return (match ? match[2] : null);
 }
 
-function applyTheme(theme)
+function	applyTheme(theme)
 {
 	document.body.classList.remove('light-theme', 'dark-theme');
 	document.body.classList.add(theme);
 
-	const sessionID = getCookieValue("sessionID");
+	const	sessionID = getCookieValue("sessionID");
 	if (sessionID)
 	{
-		fetch("/cgi_methods/update-theme.cgi",
+		return fetch("/cgi_methods/update-theme.cgi",
 		{
 			method: "POST",
 			headers: { "Content-Type": "application/x-www-form-urlencoded" },
 			body: `sessionID=${sessionID}&theme=${theme}`
 		});
 	}
+	return (Promise.resolve());
+}
+
+async function	toggleTheme()
+{
+	const current = document.body.classList.contains('light-theme') ? 'light-theme' : 'dark-theme';
+	const newTheme = current === 'light-theme' ? 'dark-theme' : 'light-theme';
+	await applyTheme(newTheme);
+	window.location.reload();
 }
 
 window.addEventListener('DOMContentLoaded', () =>
